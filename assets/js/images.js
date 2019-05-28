554e4b7b8d35dc390d7fcb2f34f57ed24c4f2f6d
Kim Eggler <kim.eggler@swisscom.com>
2019-05-28 11:04:13 +0200

    added new images and round icons

--- assets/js/images.js
+++ assets/js/images.js
@@ -10,6 +10,11 @@ window.addEventListener("load", () => {
     "https://image.tmdb.org/t/p/original/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
     "https://image.tmdb.org/t/p/original/c4Dw37VZjBmObmJw9bmt8IDwMZH.jpg",
     "https://image.tmdb.org/t/p/original/ypyeMfKydpyuuTMdp36rMlkGDUL.jpg",
-    "https://image.tmdb.org/t/p/original/gIMEoSUJK4G9cYJwew7ZERPj5Pz.jpg"
+    "https://image.tmdb.org/t/p/original/gIMEoSUJK4G9cYJwew7ZERPj5Pz.jpg",
+    "https://image.tmdb.org/t/p/original/wMFad1v8SwyVvrKXmsIkLhSxCEC.jpg",
+    "https://image.tmdb.org/t/p/original/d1hQaeKeAW3FBc3v6tIP5utleU0.jpg",
+    "https://image.tmdb.org/t/p/original/c8RuOYaTC3tEG85GtQjEtoa7qkl.jpg",
+    "https://image.tmdb.org/t/p/original/qOJRRTF9RkYDcpZZAkOv6tsXHuP.jpg",
+    "https://image.tmdb.org/t/p/original/bXBrS7gSuBz8e4svaGDXG9GDgnK.jpg"
   ];
 });
